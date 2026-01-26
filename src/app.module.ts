85d36a4419ab6a6ce1c07829f119127a79082ed0
Bastián Carrión Sandoval <67606866+PatoEladio@users.noreply.github.com>
2026-01-26 14:21:23 -0300

    para pruebas

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,12 +40,12 @@ import { ConfigModule } from '@nestjs/config';
     }),
     MailerModule.forRoot({
       transport: {
-        host: process.env.EMAIL_HOST,
+        host: 'smtp.gmail.com',
         port: 587,
         secure: false,
         auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
+          user: 'soportefemasetest@gmail.com',
+          pass: 'mskobvyknfujvshj'
         },
         tls: {
           rejectUnauthorized: false
