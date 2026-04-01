be38bad227185cbb428702c3387840867a8258e9
Bastián Carrión Sandoval <67606866+PatoEladio@users.noreply.github.com>
2026-04-01 11:53:29 -0300

    aa

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
     username: string,
     pass: string,
     req: Request
-  ): Promise<{ token: string, username: string, profile: string, profile_id: number, empresa_id: number, empresa: string }> {
+  ): Promise<{ token: string, username: string, profile: string, profile_id: number, empresa_id: number, empresa: string, num_ficha: string }> {
 
     const user = await this.usersService.searchActiveUser(username);
 
@@ -31,7 +31,7 @@ export class AuthService {
       throw new UnauthorizedException('Credenciales incorrectas');
     }
 
-    const payload = { sub: user.usuario_id, username: user.username, profile: user.perfil.perfil_id };
+    const payload = { sub: user.usuario_id, username: user.username, profile: user.perfil.perfil_id, num_ficha: user.empleado.num_ficha };
 
     // Registrar la sesión activa
     const ip = req.headers['x-forwarded-for'] as string || req.ip || 'Desconocida';
@@ -44,7 +44,8 @@ export class AuthService {
       profile_id: user.perfil.perfil_id,
       profile: user.perfil.nombre_perfil,
       empresa_id: user.empresa.empresa_id,
-      empresa: user.empresa.nombre_empresa
+      empresa: user.empresa.nombre_empresa,
+      num_ficha: user.empleado.num_ficha
     };
   }
 }
